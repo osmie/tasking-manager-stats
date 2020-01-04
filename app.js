ec8c3add5be9781a388ee0b0dfd7c73c56c12304
Rory McCann <rory@technomancy.org>
2020-01-04 20:18:11 +0100

    misc

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 "use strict";
 
+function get_tasking_manager_url() {
+    return document.getElementById("tasking_manager_url").value;
+}
+
 function html(elementName, attrs, childNodes_or_innerHTML) {
     let el = document.createElement(elementName);
     for (const [ key, value ] of Object.entries(attrs)) {
@@ -19,8 +23,7 @@ function html(elementName, attrs, childNodes_or_innerHTML) {
 async function load_tm() {
     let base = document.getElementById("base");
     base.innerHTML = "Loading...";
-    let tasking_manager_url = document.getElementById("tasking_manager_url").value;
-    console.log(tasking_manager_url);
+    let tasking_manager_url = get_tasking_manager_url();
     let resp = await fetch(tasking_manager_url+"/api/health-check");
     if (!resp.ok) {
         base.innerHTML = `Cannot connect to ${tasking_manager_url}`;
@@ -36,9 +39,11 @@ async function load_tm() {
     let campaigns = await resp.json();
     base.innerHTML = "";
 
-    base.append(
-        html("select", {"id": "campaign_name"}, campaigns.tags.map(t => html("option", {"value": t}, t)),)
-    );
+    base.append(html("div", {},
+        [ html("span", {}, "Campaigns on this server:"),
+          html("select", {"id": "campaign_name"}, campaigns.tags.map(t => html("option", {"value": t}, t))),
+        ]
+    ));
     base.append(html("button", {"onclick": "load_campaign()"}, "Load Campaign"));
     base.append(html("div", {'id': 'campaign_details'}));
 }
@@ -47,4 +52,33 @@ async function load_campaign() {
     let campaign_details = document.getElementById("campaign_details");
     let campaign = document.getElementById("campaign_name").value;
     campaign_details.innerHTML = `Loading ${campaign}...`;
+
+    let tasking_manager_url = get_tasking_manager_url();
+    let resp = await fetch(tasking_manager_url + "/api/v1/project/search?campaign_tag="+campaign);
+    let projects = await resp.json();
+
+    campaign_details.innerHTML = "";
+    campaign_details.append(html("div", {}, `There are ${projects.results.length} projects in campaign ${campaign}`));
+    campaign_details.append(html("select", {"id": "project_id"},
+        projects.results.map(p => html("option", {"value": p.projectId}, `${p.projectId} ${p.name}`))
+    ));
+    base.append(html("button", {"onclick": "load_project()"}, "Load Project"));
+    base.append(html("div", {'id': 'project_details'}));
+
+
+}
+
+async function load_project() {
+    let tasking_manager_url = get_tasking_manager_url();
+    let project_details = document.getElementById("campaign_details");
+    let project_id = document.getElementById("project_id").value;
+    let resp = await fetch(tasking_manager_url + "/api/v1/project/"+project_id+"/tasks");
+    let tasks = await resp.json();
+    console.log(tasks);
+    tasks = tasks.features.map(t => {
+        let task_id = t.properties.taskId;
+        return task_id;
+    });
+    console.log(tasks);
+
 }
