4b10f61a5456d78ffa2046894084d219fd40879a
Rory McCann <rory@technomancy.org>
2020-01-05 13:19:19 +0100

    misc

--- app.js
+++ app.js
@@ -10,15 +10,16 @@ function html(elementName, attrs, childNodes_or_innerHTML) {
         el.setAttribute(key, value);
     }
     if (typeof childNodes_or_innerHTML == 'string') {
-        el.innerHTML = childNodes_or_innerHTML;
-    } 
-    if (Array.isArray(childNodes_or_innerHTML)) {
+        el.append(childNodes_or_innerHTML);
+    } else if (Array.isArray(childNodes_or_innerHTML)) {
         for (let i=0; i<childNodes_or_innerHTML.length; i++) {
             el.append(childNodes_or_innerHTML[i]);
         }
     }
     return el;
 }
+function td(content) { return html("td", {}, content) }
+function th(content) { return html("th", {}, content) }
 
 async function load_tm() {
     let base = document.getElementById("base");
@@ -44,7 +45,7 @@ async function load_tm() {
           html("select", {"id": "campaign_name"}, campaigns.tags.map(t => html("option", {"value": t}, t))),
         ]
     ));
-    base.append(html("button", {"onclick": "load_campaign()"}, "Load Campaign"));
+    base.append(html("button", {"onclick": "load_campaign()"}, ["Load Campaign"]));
     base.append(html("div", {'id': 'campaign_details'}));
 }
 
@@ -56,29 +57,72 @@ async function load_campaign() {
     let tasking_manager_url = get_tasking_manager_url();
     let resp = await fetch(tasking_manager_url + "/api/v1/project/search?campaign_tag="+campaign);
     let projects = await resp.json();
+    
+    let all_projects = projects.results;
+    all_projects.sort((p1, p2) => { if (p1.name < p2.name) { return -1; } else if (p1.name == p2.name) { return 0; } else if (p1.name > p2.name) { return 1; }});
 
     campaign_details.innerHTML = "";
     campaign_details.append(html("div", {}, `There are ${projects.results.length} projects in campaign ${campaign}`));
     campaign_details.append(html("select", {"id": "project_id"},
-        projects.results.map(p => html("option", {"value": p.projectId}, `${p.projectId} ${p.name}`))
+        all_projects.map(p => html("option", {"value": p.projectId}, `${p.projectId} ${p.name}`))
     ));
-    base.append(html("button", {"onclick": "load_project()"}, "Load Project"));
-    base.append(html("div", {'id': 'project_details'}));
+    campaign_details.append(html("button", {"onclick": "load_project()"}, "Load Project"));
+    campaign_details.append(html("div", {'id': 'project_details'}));
 
 
 }
 
 async function load_project() {
+    let project_details = document.getElementById("project_details");
+
+    let resp = await fetch(tasking_manager_url + "/api/v1/project/"+project_id);
+    let project = await resp.json();
+
     let tasking_manager_url = get_tasking_manager_url();
-    let project_details = document.getElementById("campaign_details");
     let project_id = document.getElementById("project_id").value;
-    let resp = await fetch(tasking_manager_url + "/api/v1/project/"+project_id+"/tasks");
-    let tasks = await resp.json();
-    console.log(tasks);
-    tasks = tasks.features.map(t => {
-        let task_id = t.properties.taskId;
-        return task_id;
-    });
-    console.log(tasks);
+    resp = await fetch(tasking_manager_url + "/api/v1/stats/project/"+project_id+"/activity");
+    let activity = await resp.json();
+
+    let max_pages = activity.pagination.pages;
+    let history_load_progress = html("span", {}, ["Loading...", html("progress", {"id": "loading_history", "max":max_pages, "value": 0})]);
+    project_details.append(history_load_progress);
+
+    // TODO replace this with some sort of parallel fetch/Promise
+    let all_history = []
+    for (let page =1; page<=max_pages; page++) {
+        resp = await fetch(tasking_manager_url + "/api/v1/stats/project/"+project_id+"/activity?page="+page);
+        let hist_page = await resp.json();
+        for (var i=0; i<hist_page.activity.length; i++) {
+            let a = hist_page.activity[i];
+            if (a.action == 'STATE_CHANGE' && a.actionText == 'MAPPED') {
+                all_history.push(a);
+            }
+        }
+        history_load_progress.setAttribute("value", page);
+    }
+
+    history_load_progress.remove();
+
+    let project_created = new Date(project.created);
+    let now = new Date();
+    let mapping_events = all_history.map(e => new Date(e.actionDate));
+    mapping_events.sort();
+
+    console.log(mapping_events);
+
+
+    let project_age = now - project_created;
+
+    project_details.append(html("div", {},
+        [ "Mapping Rate", 
+            html("table", {},
+            [
+                html("tr", {}, [ th("Since When"), th("Rate"), th("Est. Finished") ] ),
+                html("tr", {}, [ td("Since start"), td("N/A"), td("N/A") ] ),
+            ])
+        ]));
+    
 
 }
+
+function 
